20d9b3e10b6d129a691e27f0ea608fd8e619662e
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2024-03-14 19:10:09 +0530

    Create Layout.js

---
+++ client/components/Layout.js
@@ -0,0 +1,15 @@
+import { UserProvider } from '../context'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+    return (
+        <UserProvider>
+            <Navbar />
+            {children}
+            <Footer />
+        </UserProvider>
+    )
+}
+
+export default Layout
